0028c48f97d56e5fb0af9a384ed25cc39b0f1875
Ian Suvak <ian.suvak@avalabs.org>
2025-06-13 11:08:24 -0400

    remove stray unit test

--- signerserver/token_test.go
+++ signerserver/token_test.go
@@ -1,17 +1,12 @@
 package signerserver
 
 import (
-	"context"
-	"crypto/rand"
 	"encoding/json"
 	"fmt"
 	"testing"
 
-	"github.com/ava-labs/avalanchego/utils/crypto/bls/signer/rpcsigner"
 	"github.com/ava-labs/cubist-signer/api"
 	"github.com/stretchr/testify/require"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 const (
@@ -92,21 +87,4 @@ func TestTokenDataMarshalJSON(t *testing.T) {
 	require.NoError(err)
 
 	require.EqualValues(originalMap, marshaledMap)
-}
-
-func TestConn(t *testing.T) {
-	clientConn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
-	require.NoError(t, err)
-
-	signerClient, err := rpcsigner.NewClient(context.Background(), clientConn)
-	require.NoError(t, err)
-
-	// generate random bytes
-	msg := make([]byte, 32)
-	_, err = rand.Read(msg)
-	require.NoError(t, err)
-
-	sig, err := signerClient.Sign(msg)
-	require.NoError(t, err)
-	require.NotNil(t, sig)
 }
